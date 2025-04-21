a96e61e795f10b03220c46598a25ec4b08de0ca1
sarik1970 <aborshchenko@yahoo.com>
2025-04-21 11:07:20 -0700

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,6 +1,6 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Alex </h1>
     <p>
       You can use me to create your own chat bot app. In this demo I am an
       expert in movie reviews, so feel free to ask me about movies! Then, create
